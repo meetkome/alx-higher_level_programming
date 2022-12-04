261bad7621222e7c894c133c672697fdb0b4e1f7
meetkome <kvictory1010@gmail.com>
2022-12-05 00:43:34 +0100

    new

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
-    for r in rows:
-        print(r)
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
